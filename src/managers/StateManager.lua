26e0ede259b5913a0b5df36f9eeedc7d84cecf47
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-10-30 23:12:11 +0100

    Added StateManager

---
+++ src/managers/StateManager.lua
@@ -0,0 +1,38 @@
+StateManager = Class {}
+
+function StateManager:init()
+    self.empty = {
+        update = function(dt) end,
+        draw   = function() end,
+        enter  = function(params) end,
+        exit   = function() end,
+    }
+    self.states = {}
+    self.current = self.empty
+end
+
+function StateManager:change(stateName, enterParams)
+    assert(self.states[stateName])
+    self.current:exit()
+    self.current = self.states[stateName]()
+    self.current:enter(enterParams)
+end
+
+function StateManager:update(dt)
+    self.current:update(dt)
+end
+
+function StateManager:draw()
+    self.current:draw()
+end
+
+function StateManager:switchTo(stateName, enterParams)
+    assert(self.states[stateName])
+    self.placeholder = self.current
+    self:change(stateName, enterParams)
+end
+
+function StateManager:switchBack()
+    self.current:exit()
+    self.current = self.placeholder
+end
