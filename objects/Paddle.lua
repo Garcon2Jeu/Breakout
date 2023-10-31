f309a6b3b9aa08f7f972c0fd1dfac9c5d7dfcbb7
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-10-31 16:15:47 +0100

    Added Paddle

---
+++ objects/Paddle.lua
@@ -0,0 +1,11 @@
+Paddle = Class {}
+
+function Paddle:init() end
+
+function Paddle:update(dt) end
+
+function Paddle:draw() end
+
+function Paddle:enter(params) end
+
+function Paddle:exit() end
--- src/states/ServeState.lua
+++ src/states/ServeState.lua
@@ -1,4 +1,4 @@
-ServeState = Class {}
+ServeState = Class { __includes = BaseState }
 
 function ServeState:init() end
 
