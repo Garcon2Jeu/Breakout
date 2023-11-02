d4fc57abc29ace07a6294f2fc89986f82d6118c5
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-11-02 17:16:16 +0100

    Added PlayerStats

---
+++ objects/PlayerStats.lua
@@ -0,0 +1,11 @@
+PlayerStats = Class {}
+
+function PlayerStats:init()
+    self.level = 1
+    self.score = 0
+    self.hearts = 3
+end
+
+function PlayerStats:update(dt) end
+
+function PlayerStats:draw() end
